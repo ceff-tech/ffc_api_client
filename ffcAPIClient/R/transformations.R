fe9b34876abd1eed3d32be9196d40208a669be18
Nick Santos <nickrsan@users.noreply.github.com>
2019-12-11 19:42:04 -0800

    Changed "mean" to "median" in DRH plot text

--- ffcAPIClient/R/transformations.R
+++ ffcAPIClient/R/transformations.R
@@ -36,8 +36,8 @@ plot_drh <- function(results, output_path){
     ggplot2::geom_line(data=drh_data, ggplot2::aes(x=days, y=fifty), color="black", lwd=1.2) +
     ggplot2::theme_classic(base_family = "Roboto Condensed") +
     ggplot2::labs(title="Dimensionless Hydrograph", x="Julian Day",
-         y="Daily mean flow / Avg annual flow",
-         caption="Daily mean flow with 10/90 percentiles (light blue), and 25/75 percentiles in purple")
+         y="Daily median flow / Avg annual flow",
+         caption="Daily median flow with 10/90 percentiles (light blue), and 25/75 percentiles in purple")
 
   if(!is.null(output_path)){
     ggplot2::ggsave(filename = output_path, width = 7, height = 5, units = "in", dpi=300)
