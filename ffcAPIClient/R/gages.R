abb528de4827b72a43043974b57513c389071176
Nick Santos <nickrsan@users.noreply.github.com>
2019-12-13 18:49:53 -0800

    Upgrading Gage Class to retrieve COMID
    See #7

--- ffcAPIClient/R/gages.R
+++ ffcAPIClient/R/gages.R
@@ -1,11 +1,69 @@
+#' USGS Gage Retrieval Tools
+#'
+#' This class retrieves data for a USGS gage.
+#'
+#' @examples
+#' gage$gage_id <- 11427000
+#' gage <- ffcAPIClient::USGSGage$new()
+#' gage$gage_id <- 11427000
+#' gage$get_data()
+#' gage$get_comid_for_gage()
+#' gage$comid
+#' [1] 14996611
+#' ffcAPIClient::get_predicted_flow_metrics(gage$comid)
+#'                 Metric    COMID          p10          p25        p50          p75          p90 source
+#' 70804        DS_Dur_WS 14996611 1.051875e+02 1.273438e+02   154.0625 1.785563e+02 1.953908e+02  model
+#' 211050       DS_Mag_50 14996611 4.998793e+01 6.732828e+01   104.4028 1.464183e+02 1.882733e+02  model
+#' 351296       DS_Mag_90 14996611 9.314097e+01 1.291930e+02   173.6844 2.382053e+02 3.393799e+02  model
+#' 491542          DS_Tim 14996611 2.720000e+02 2.823875e+02   296.8875 3.070000e+02 3.210167e+02  model
+#' 586665          FA_Dur 14996611 2.000000e+00 3.000000e+00     4.0000 6.000000e+00 8.000000e+00    obs
+#' 702508          FA_Mag 14996611 1.294269e+02 1.886283e+02   289.6838 4.540329e+02 8.514823e+02  model
+#' 842754          FA_Tim 14996611 7.816667e+00 1.400000e+01    24.6250 2.900000e+01 4.217000e+01  model
+#' 983000         Peak_10 14996611 1.243107e+04 1.947545e+04 22830.3355 3.124928e+04 3.767889e+04  model
+#' 1123246        Peak_20 14996611 8.078893e+03 1.227363e+04 20218.4829 2.087196e+04 2.087196e+04  model
+#' 1263492        Peak_50 14996611 3.532988e+03 7.350986e+03  8542.1191 8.969386e+03 8.969386e+03  model
+#' 1358615    Peak_Dur_10 14996611 1.000000e+00 1.000000e+00     1.0000 2.000000e+00 4.000000e+00    obs
+#' 1429335    Peak_Dur_20 14996611 1.000000e+00 1.000000e+00     2.0000 3.000000e+00 6.000000e+00    obs
+#' 1500055    Peak_Dur_50 14996611 1.000000e+00 1.000000e+00     4.0000 1.000000e+01 2.900000e+01    obs
+#' 1570775    Peak_Fre_10 14996611 1.000000e+00 1.000000e+00     1.0000 1.000000e+00 2.000000e+00    obs
+#' 1641495    Peak_Fre_20 14996611 1.000000e+00 1.000000e+00     1.0000 2.000000e+00 3.000000e+00    obs
+#' 1712215    Peak_Fre_50 14996611 1.000000e+00 1.000000e+00     2.0000 3.000000e+00 5.000000e+00    obs
+#' 1828058         SP_Dur 14996611 4.700000e+01 5.900000e+01    72.0000 9.527500e+01 1.215417e+02  model
+#' 1968304         SP_Mag 14996611 1.067727e+03 1.662598e+03  2489.0563 3.771512e+03 5.809320e+03  model
+#' 2063427         SP_ROC 14996611 3.845705e-02 4.863343e-02     0.0625 8.132020e-02 1.141117e-01    obs
+#' 2179270         SP_Tim 14996611 1.607717e+02 1.905000e+02   218.7500 2.354750e+02 2.447583e+02  model
+#' 2319516    Wet_BFL_Dur 14996611 7.633333e+01 1.073000e+02   141.1958 1.633750e+02 1.875000e+02  model
+#' 2459762 Wet_BFL_Mag_10 14996611 1.519943e+02 1.960031e+02   278.2581 4.384614e+02 5.489183e+02  model
+#' 2600008 Wet_BFL_Mag_50 14996611 4.148992e+02 5.902507e+02   924.1728 1.175461e+03 1.426576e+03  model
+#' 2740254        Wet_Tim 14996611 4.937500e+01 5.905000e+01    73.0000 8.835625e+01 1.035083e+02  model
+#'
+#' @export
 USGSGage <- R6::R6Class("USGSGage", list(
+
   gage_id = NA,
+  comid = NA,  # The COMID of the stream segment this gage is on.
   timeseries_data = NA,
-  validate = function(){
+  latitude = NA,
+  longitude = NA,
+
+  #' Validates that gage is ready to run requests
+  #'
+  #' Internal method. Checks parameters to make sure they're ready for other methods on the object.
+  validate = function(latlong){
+    if(missing(latlong)){
+      latlong = FALSE
+    }
+
     if(is.na(self$gage_id)){
       stop("Must set gage_id property of USGSGage before calling get_data")
     }
+
+    if(latlong == TRUE && (is.na(self$latitude) || is.na(self$longitude))){
+      stop("Latitude or Longitude is missing for gage, but required. Either set it manually, or
+           run get_data, which should set it automatically.")
+    }
   },
+
   get_data = function(){
 
     self$validate()
@@ -14,6 +72,11 @@ USGSGage <- R6::R6Class("USGSGage", list(
     usgs_daily_1 <- dataRetrieval::whatNWISdata(siteNumber=self$gage_id, service='dv',
                              parameterCd = '00060',
                              statCd="00003")
+
+    # save the latitude and longitude
+    self$latitude <- usgs_daily_1$dec_lat_va
+    self$longitude <- usgs_daily_1$dec_long_va
+
     usgs_daily_2 <- dplyr::select(usgs_daily_1, site_no, station_nm, dec_lat_va, dec_long_va,
            dec_coord_datum_cd, alt_va, huc_cd, data_type_cd,
            parm_cd, stat_cd, begin_date:count_nu)
@@ -40,10 +103,35 @@ USGSGage <- R6::R6Class("USGSGage", list(
 
     self$timeseries_data <- daily_df
     invisible(self)
-
   },
+
+  #' Looks up the COMID for this gage
+  #'
+  #' This method looks up the COMID for the gage and sets the comid attribute. It does not return
+  #' the COMID. It returns this object for chaining. The gage's gage_id, latitude, and longitude
+  #' attributes must be set before running this. latitude and longitude can be set manually,
+  #' or by running get_data().
+  #'
   get_comid_for_gage = function(){
-    self$validate()
+    self$validate(latlong=TRUE)
+
+    if(length(nhdR::nhd_plus_list(vpu=18)) == 0){  # checks if the NHDPlus Data has already been downloaded
+      nhdR::nhd_plus_get(vpu=18)  # downloads and caches it for use
+    }
+
+    # This does all the spatial stuff for us automatically - basically does a spatial join at this gage's
+    # latitude and longitude to find the stream segments within 100 meters. Then we'll pull the COMIDs
+    spatial_qry <- nhdR::nhd_plus_query(self$longitude,
+                                self$latitude,
+                                dsn = c("NHDFlowline"),
+                                buffer_dist = units::as_units(100, "m"))
+
+    self$comid <- spatial_qry$sp$NHDFlowline[1]$COMID  # gets the first COMID returned
+    invisible(self)  # return itself invisibly, but after setting the COMID
+  },
+
+  get_predicted_metrics = function(){
+    return(get_predicted_flow_metrics(self$comid))
   }
 ))
 
