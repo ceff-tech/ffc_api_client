b01c02c1414762ef376c9af5ac9f0468af273ac4
Nick Santos <nickrsan@users.noreply.github.com>
2020-01-10 14:06:53 -0800

    testing solution for using external data internally for flow_metrics

--- ffcAPIClient/R/utils_flow_metrics.R
+++ ffcAPIClient/R/utils_flow_metrics.R
@@ -12,13 +12,16 @@
 #'
 #' @export
 get_predicted_flow_metrics <- function(com_id){
+  fm_data_env <- new.env()
+  data("flow_metrics", envir=fm_data_env)
+  flow_metrics <- get("flow_metrics", envir=fm_data_env)
   return(flow_metrics[flow_metrics$COMID == com_id, ])
 }
 
 #' Retrieves COMID for a given USGS gage which collects daily data.
 #'
 #' This function returns the COMID associated with a specific USGS gage.
-#' It can be used to associate gage data with flow metric predictions a 
+#' It can be used to associate gage data with flow metric predictions a
 #' stream segment identified with the \code{com_id} input variable.
 #'
 #' @param gage_id character. A character formatted 8 digit USGS Gage ID.
@@ -37,12 +40,12 @@ get_comid_for_usgs_gage <- function(gage_id){
 #' Retrieves COMID for a given USGS gage which collects daily data.
 #'
 #' This function returns the COMID associated with a specific USGS gage.
-#' It can be used to associate gage data with flow metric predictions a 
+#' It can be used to associate gage data with flow metric predictions a
 #' stream segment identified with the \code{com_id} input variable.
 #'
 #' @param longitude numeric. Longitude or X.
 #' @param latitude numeric. Longitude or Y.
-#' 
+#'
 #' @export
 get_comid_for_lon_lat <- function(longitude, latitude){
   start_point <- sf::st_sfc(sf::st_point(c(longitude, latitude)), crs = 4269)
