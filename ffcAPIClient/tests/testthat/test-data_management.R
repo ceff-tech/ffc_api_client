767f7102b0dd0bbf2067abe1c55cf21f483b55c7
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-01-15 20:57:13 -0800

    Fix for unit test accessing private environment in CI

--- ffcAPIClient/tests/testthat/test-data_management.R
+++ ffcAPIClient/tests/testthat/test-data_management.R
@@ -8,7 +8,7 @@ test_that("Shouldn't exist before we load it", {
 
 test_that("Succeeds at basic loading", {
   get_dataset("stream_class_data")
-  expect_true(exists("stream_class_data", envir = ffcAPIClient::ffcAPIClient_data_env))
+  expect_true(exists("stream_class_data", envir = ffcAPIClient_data_env))
   expect_equal(get_stream_class_code_for_comid(17636786), "LSR")
   expect_equal(length(get_stream_class_code_for_comid(9838328932893289523)), 0)  # confirm we get an item of length 0 back if we put in something gibberishy
 })
